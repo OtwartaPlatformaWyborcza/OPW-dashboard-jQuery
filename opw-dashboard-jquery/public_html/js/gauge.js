9815eb1ae46c0052366c9ec625f553da909f9e54
Kacper <jasinski.it@gmail.com>
2015-04-13 15:00:22 +0200

    gauge gradient

--- opw-dashboard-jquery/public_html/js/gauge.js
+++ opw-dashboard-jquery/public_html/js/gauge.js
@@ -1,34 +1,41 @@
 function gauge( procent ) {
-	//canvas initialization
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
-	//dimensions
-	var W = canvas.width;
-	var H = canvas.height;
-	//Variables
-	var degrees = ( procent * 180) / 100;
-	var new_degrees = 0;
-	var difference = 0;
-	var color = "#D9534F";
-	var bgcolor = "#EEE";
-	var text;
-	var animation_loop, redraw_loop;
-	
-	//Background arc
-	ctx.beginPath();
-	ctx.strokeStyle = bgcolor;
-	ctx.lineWidth = 40;
-	ctx.arc( W / 2, H / 2, 100, Math.PI, 0, false );
-	ctx.stroke();
 
-	//Main arc
-	//degrees = 90;
-	var radians = degrees * Math.PI / 180;
-	radians = Math.PI - ( procent * Math.PI / 100 );
-	ctx.beginPath();
-	ctx.strokeStyle = color;
-	ctx.lineWidth = 30;
-	//ctx.arc(x,y,radius,start_angle, end_angle, ??);
-	ctx.arc( W/2, H/2, 100, Math.PI, -radians, false);
-	ctx.stroke();
+    //canvas initialization
+    var canvas = document.getElementById( "canvas" );
+    var ctx = canvas.getContext( "2d" );
+
+    //dimensions
+    var W = canvas.width;
+    var H = canvas.height;
+
+    //Variables
+    var newDegrees = 0;
+    var difference = 0;
+    var color = "#D9534F";
+    var bgcolor = "#EEE";
+    var wow = "hsl(" + procent * 1.17 + ", 56.4%, 45.9%)";
+    var gradient = ctx.createLinearGradient( 0, 0, W, 0 );
+    gradient.addColorStop( 0, "hsl(0, 56.4%, 45.9%)" );
+    gradient.addColorStop( 0.5, "#F0AD4E" );
+    gradient.addColorStop( 1, "green" );
+    var text;
+    var animationLoop, redrawLoop;
+
+    //Background arc
+    ctx.beginPath();
+    ctx.strokeStyle = gradient;
+    ctx.lineWidth = 30;
+    ctx.arc( W / 2, H / 2, 100, Math.PI, 0, false );
+    ctx.stroke();
+
+    //Main arc
+    if ( procent === 100 ) {
+        var radians = 0;
+    } else {
+        var radians = Math.PI + ( procent * Math.PI / 100 );
+    }
+    ctx.beginPath();
+    ctx.strokeStyle = bgcolor;
+    ctx.arc( W / 2, H / 2, 100, 0, radians, true );
+    ctx.stroke();
 }

6f7c096e264e6dad5d3476395947c4a24a027624
Kacper <jasinski.it@gmail.com>
2015-04-23 01:12:52 +0200

    Removed magic switch/case

--- opw-dashboard-jquery/public_html/js/config.js
+++ opw-dashboard-jquery/public_html/js/config.js
@@ -8,16 +8,16 @@ var cfg = {
         tooltip: {isHtml: true},
         legend: 'none',
         hAxis: { slantedText:true, slantedTextAngle:20 },
-        vAxes: {
-            0: {format: '#%', title: 'Wynik procentowy', titleTextStyle: {italic: false}},
-            1: {title: 'Liczba głosów', titleTextStyle: {italic: false}},
-            2: {textPosition: 'none'}
-        },
+        vAxis: {format: '#%'},
+        //chartArea: {width: '85%'},
         seriesType: "bars",
-        series: {
-            0: {type: "bars", targetAxisIndex: 0},
-            1: {type: "line", targetAxisIndex: 2, color: "none", enableInteractivity: false},
-            2: {type: "line", targetAxisIndex: 2, enableInteractivity: false}
-        }
     }
 };
+
+var okrInWoj = [
+    [1,2,3,4], [5,6,7], [8,9,10,11],
+    [12,13], [14,15,16,17], [18,19,20],
+    [21,22,23,24,25,26], [27], [28,29,30,31],
+    [32,33,34], [35,36,37,38], [39,40,41],
+    [42], [43,44], [45,46,47,48,49], [50,51]
+];
--- opw-dashboard-jquery/public_html/js/plot2.js
+++ opw-dashboard-jquery/public_html/js/plot2.js
@@ -2,103 +2,54 @@ google.load('visualization', '1.0', {'packages':['corechart', 'gauge', 'controls
 
 
 function wojewodztwa(data) {
-
     var woj = {
-        0: [0,0,0,0],
-        1: [0,0,0,0],
-        2: [0,0,0,0],
-        3: [0,0,0,0],
-        4: [0,0,0,0],
-        5: [0,0,0,0],
-        6: [0,0,0,0],
-        7: [0,0,0,0],
-        8: [0,0,0,0],
-        9: [0,0,0,0],
-        10: [0,0,0,0],
-        11: [0,0,0,0],
-        12: [0,0,0,0],
-        13: [0,0,0,0],
-        14: [0,0,0,0],
-        15: [0,0,0,0]
+        0: [0,0,0,0], 1: [0,0,0,0], 2: [0,0,0,0], 3: [0,0,0,0],
+        4: [0,0,0,0], 5: [0,0,0,0], 6: [0,0,0,0], 7: [0,0,0,0],
+        8: [0,0,0,0], 9: [0,0,0,0], 10: [0,0,0,0], 11: [0,0,0,0],
+        12: [0,0,0,0], 13: [0,0,0,0], 14: [0,0,0,0], 15: [0,0,0,0]
     }
 
-    var name = ['dolnośląskie','kujawsko-pomorskie','lubelskie','lubuskie','łódzkie','małopolskie','mazowieckie','opolskie','podkarpackie','podlaskie','pomorskie','śląskie','świętokrzyskie','warmińsko-mazurskie','wielkopolskie','zachodniopomorskie']
+    var wojName = ['dolnośląskie','kujawsko-pomorskie','lubelskie','lubuskie',
+    'łódzkie','małopolskie','mazowieckie','opolskie',
+    'podkarpackie','podlaskie','pomorskie','śląskie',
+    'świętokrzyskie','warmińsko-mazurskie','wielkopolskie','zachodniopomorskie'];
 
-    var id,z;
+    var idOkr, idWoj=0;
     for (var i in data)
     {
-        id = parseInt( (data[i].okregowaName.match(/\d{1,2}/)) );
-        switch(true) {
-            case (id<5):
-                z=0;
+        idOkr = parseInt( (data[i].okregowaName.match(/\d{1,2}/)) );
+        for (var j = 0; j < okrInWoj.length; j++) {
+            if (okrInWoj[j].indexOf(idOkr) >= 0) {
+                idWoj = j;
                 break;
-            case (id<8):
-                z=1;
-                break;
-            case (id<12):
-                z=2;
-                break;
-            case (id<14):
-                z=3;
-                break;
-            case (id<18):
-                z=4;
-                break;
-            case (id<21):
-                z=5;
-                break;
-            case (id<27):
-                z=6;
-                break;
-            case (id<28):
-                z=7;
-                break;
-            case (id<32):
-                z=8;
-                break;
-            case (id<35):
-                z=9;
-                break;
-            case (id<39):
-                z=10;
-                break;
-            case (id<42):
-                z=11;
-                break;
-            case (id<43):
-                z=12;
-                break;
-            case (id<45):
-                z=13;
-                break;
-            case (id<50):
-                z=14;
-                break;
-            case (id<52):
-                z=15;
-                break;
+            }
         }
-        woj[z][0]+=data[i].frekwencja;
-        woj[z][1]+=data[i].frekwencjaAll;
-        woj[z][2]+=data[i].obwodowa;
-        woj[z][3]+=data[i].obwodowaAll;
+        woj[idWoj][0]+=data[i].frekwencja;
+        woj[idWoj][1]+=data[i].frekwencjaAll;
+        woj[idWoj][2]+=data[i].obwodowa;
+        woj[idWoj][3]+=data[i].obwodowaAll;
     }
 
-    console.log(woj);
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Województwo');
-    data.addColumn('number', 'Frekwencja');
-    data.addColumn('number', 'Otrzymane protokoły');
+    data.addColumn('number', 'Frekwencja (procentowo)');
+    data.addColumn('number', 'Otrzymane protokoły (procentowo)');
 
+    var frekw, prot;
     for (var i in woj) {
-        data.addRows( [ [ name[i], Math.round( woj[i][0] * 10000 / woj[i][1] )/100, Math.round(( woj[i][2] * 10000 ) / woj[i][3])/100 ] ] );
+        frekw = Math.round( woj[i][0] *100/ woj[i][1]);
+        prot = Math.round(( woj[i][2] * 100 ) / woj[i][3]);
+        data.addRows([[ wojName[i], frekw, prot ]]);
     }
 
     var options = {
         region: 'PL',
         resolution: 'provinces',
         colorAxis: {colors: ['#B0D794', '#4C6472']},
-        backgroundColor: '#337AB7'
+        backgroundColor: '#337AB7',
+        legend: {
+            numberFormat: "#.#'%"
+        }
     };
 
     var chart = new google.visualization.GeoChart(document.getElementById('wykres4'));
@@ -106,19 +57,17 @@ function wojewodztwa(data) {
     chart.draw(data, options);
 }
 
+// Rysowanie wykresu dla kandydatow
 function prezydent(data) {
     var dataChart = new google.visualization.DataTable();
     dataChart.addColumn('string', 'kandydat');
     dataChart.addColumn('number', 'wynik procentowy');
     dataChart.addColumn({type: 'string', role: 'annotation'});
     dataChart.addColumn({type: 'string', role: 'tooltip', 'p': {'html':true}});
-    dataChart.addColumn('number', 'glosow');
-    dataChart.addColumn('number', 'linia');
-
     var suma = 0;
     for (i in data)
         suma+=data[i].glosow;
-    console.log(suma);
+
     var procent, nazwisko, etykieta, opis, imieNazwisko;
     for (i in data) {
         procent =  ( ( data[i].glosow * 100 ) / suma ) / 100;
@@ -126,7 +75,7 @@ function prezydent(data) {
         nazwisko = data[i].lastname.toUpperCase();
         opis = "<b>" + imieNazwisko + " </b>Głosów:&nbsp;" + data[i].glosow.toString() +  "</b>";
         etykieta =  Math.round(procent*100) + '%';
-        dataChart.addRows( [ [ nazwisko, procent, etykieta, opis, data[i].glosow, suma/2 ] ] );
+        dataChart.addRows( [ [ nazwisko, procent, etykieta, opis ] ] );
     }
 
     dataChart.sort([{column: 0}]);
@@ -142,7 +91,7 @@ function prezydent(data) {
 
 }
 
-
+// Rysowanie gauge
 function gauge(a, b) {
     var data = google.visualization.arrayToDataTable([
         ['Label', 'value'],
@@ -183,6 +132,8 @@ $( document ).ready( function() {
                 prezydent(data.kandydatList);
                 gauge(data.obwodowaAll, data.obwodowa);
                 wojewodztwa(data.okregowaList);
+                var aktualizacja = data.exportDate.substring( 11, 19 );
+                $( "#aktualizacja" ).append( "<span>" + aktualizacja + "</span>" );
             }
     });
 });

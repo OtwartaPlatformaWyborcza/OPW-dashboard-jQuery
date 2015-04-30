e84d86f83c5d13442f3f9b6ea1a7be1266832e15
Kacper <jasinski.it@gmail.com>
2015-04-30 23:22:23 +0200

    test

--- opw-dashboard-jquery/public_html/js/config.js
+++ opw-dashboard-jquery/public_html/js/config.js
@@ -1,4 +1,4 @@
-var debug = false;
+﻿var debug = false;
 var token = "d171794c5c1f7a50aeb8f7056ab84a4fbcd6fbd594b1999bddaefdd03efc0591";
 var jsonURL = "http://91.250.114.134:8080/opw/service/wynik/complete";
 

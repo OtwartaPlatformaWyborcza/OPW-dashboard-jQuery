6b2cf2bdc4149e704533523a73c0831c93010300
Adam Kowalewski <github@adamkowalewski.com>
2015-05-08 23:32:42 +0200

    test production url

--- opw-dashboard-jquery/public_html/js/config.js
+++ opw-dashboard-jquery/public_html/js/config.js
@@ -1,6 +1,6 @@
 ﻿var debug = false;
 var token = "d171794c5c1f7a50aeb8f7056ab84a4fbcd6fbd594b1999bddaefdd03efc0591";
-var jsonURL = "http://91.250.114.134:8080/opw/service/wynik/complete";
+var jsonURL = "http://otwartapw.pl/opw/service/wynik/complete";
 
 var cfg = {
     vertical: {
